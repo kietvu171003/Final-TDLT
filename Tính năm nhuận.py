14bcf0954c878a03c45135431c93392fd13a29d1
TrucVu6868 <96802816+TrucVu6868@users.noreply.github.com>
2021-12-29 14:42:56 +0700

    Delete Tính năm nhuận.py

--- Tính năm nhuận.py
+++
@@ -1,8 +0,0 @@
-def Tinh_Toan(Day, Month, Year, Hour, Minute, Second):
-    Dict_Phu=dict(Nam)
-    if Year%4==0 and Year%100!=0:
-        Dict_Phu[2]+=1
-        Tong=366
-    elif Year%400==0:
-        Dict_Phu[2]+=1
-        Tong=366
